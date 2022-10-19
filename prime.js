8e17c1d4c6bb1687125aa822af5d91ee19bd44b5
Bhusham <bhushan@gmail.com>
2022-10-20 00:12:00 +0530

    chang

--- prime.js
+++ prime.js
@@ -1,8 +1,12 @@
-let N = 11;
+let n = 11;
 let count = 0;
 
 for(let i=0; i<=N; i++){
+<<<<<<< HEAD
 if(N%i==0) {
+=======
+if(n%2==0) {
+>>>>>>> e8dbb8e14fe79d8fee58c1922da0a4a31be39226
 count++
 }
 }
